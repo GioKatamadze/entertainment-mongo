7d632f07dd9372bf6af4b9875b96ed7d87bee95a
Gio Katamadze <giokat.inbox@gmail.com>
2023-01-30 02:57:44 +0400

    remove error middleware

--- src/server.js
+++ src/server.js
@@ -8,8 +8,6 @@ import connectToMongo from "./config/mongo.js";
 import userRoutes from "./routes/userRoutes.js";
 import movieRouter from "./routes/movieRouter.js";
 import swaggerMiddleware from "./middlewares/swagger-middleware.js";
-import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
-const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -33,16 +31,6 @@ app.use("/images", express.static("public/storage"));
 app.use("/api", movieRouter);
 app.use("/api/user", userRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-}
-
-app.use(notFound);
-app.use(errorHandler);
 app.use("/", cors(), ...swaggerMiddleware());
 
 app.listen(process.env.PORT || 5000);
